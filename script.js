e2c683725c1d789bb6c3ab5ae0401c1032712371
Luan Vinicius <luanllacraft@gmail.com>
2024-09-09 19:04:45 -0300

    Atualizado com login e carrinho

---
+++ script.js
@@ -0,0 +1,9 @@
+const carrinho = document.querySelector('.cestinha-conteiner')
+
+function abrirCestinha(){
+    carrinho.classList.add('active')
+}
+
+function fechandoCesta(){
+    carrinho.classList.remove('active')
+}
